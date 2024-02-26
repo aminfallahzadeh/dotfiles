1d485a60a601804ead322a897ee2feed5a6cffbf
amin-dev-tech <amin.dev.tech@gmail.com>
2024-02-27 00:35:07 +0300

    add incline rounded corners

--- .config/nvim/lua/plugins/incline.lua
+++ .config/nvim/lua/plugins/incline.lua
@@ -13,15 +13,24 @@ return {
 			},
 			render = function(props)
 				local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+				if filename == "" then
+					filename = "[No Name]"
+				end
 				local ft_icon, ft_color = devicons.get_icon_color(filename)
 				local modified = vim.bo[props.buf].modified
-
+				local bg_color = "#ebbcba"
 				return {
-					ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-					" ",
-					{ filename, gui = modified and "bold,italic" or "bold" },
-					" ",
-					guibg = "#44406e",
+					{ "", guifg = ft_color or bg_color },
+					{
+						ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) }
+						or "",
+						" ",
+						{ filename, gui = modified and "bold,italic" or "bold" },
+						" ",
+						guibg = bg_color,
+						guifg = "#000000",
+					},
+					{ "", guifg = bg_color },
 				}
 			end,
 		})
