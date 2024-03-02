9454bf94f2e61d4b38c97a2eeea169ee7d885fdd
amin-dev-tech <amin.dev.tech@gmail.com>
2024-03-03 01:59:50 +0300

    add new confg to nvim

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,63 +1,17 @@
--- return {
 return {
 	"folke/noice.nvim",
-	event = "VimEnter",
-	lazy = true,
-	opts = {
-		lsp = {
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			},
-			signature = {
-				enabled = false,
-			},
-			progress = {
-				enabled = false,
-			},
-			message = {
-				enabled = false,
-			},
-			hover = {
-				enabled = false,
-			},
-		},
-		presets = {
-			long_message_to_split = true,
-			lsp_doc_border = true,
-			bottom_search = false,
-		},
-		routes = {
-			{
-				filter = {
-					event = "notify",
-					min_height = 10,
-				},
-				view = "split",
-			},
-			{
-				filter = {
-					event = "msg_show",
-					any = {
-						{ find = "%d+L, %d+B" },
-						{ find = "; after #%d+" },
-					},
-				},
-				view = "mini",
-			},
-		},
-		cmdline = {
-			format = {
-				cmdline = { pattern = "^:", icon = "󰞷", lang = "vim" },
-			},
-		},
-		views = {
-			mini = {
-				win_options = {
-					winblend = 0,
-				},
-			},
-		},
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
 	},
+
+	config = function()
+		require("noice").setup({
+
+			presets = {
+				bottom_search = false,
+			},
+		})
+	end,
 }
--- .config/nvim/lua/plugins/notify.lua
+++ .config/nvim/lua/plugins/notify.lua
@@ -1,56 +1,14 @@
 return {
 	"rcarriga/nvim-notify",
-
+	event = "VeryLazy",
 	opts = {
-		timeout = 3000,
-		max_height = function()
-			return math.floor(vim.o.lines * 0.75)
-		end,
-		max_width = function()
-			return math.floor(vim.o.columns * 0.75)
-		end,
-		on_open = function(win)
-			vim.api.nvim_win_set_config(win, { zindex = 100 })
-		end,
+		timeout = 5000,
+		background_colour = "#000000",
+		render = "wrapped-compact",
 	},
-	lazy = true,
 	config = function()
-		vim.opt.termguicolors = true
-		vim.notify = require("notify")
-		local notify_status_ok, notify = pcall(require, "notify")
-		if not notify_status_ok then
-			print("notify not found!")
-		end
-
-		---@diagnostic disable-next-line: missing-fields
-		notify.setup({
-			minimum_width = 50,
-			render = "default",
-			stages = "fade",
-			timeout = 2000,
-			fps = 30,
-			max_height = function()
-				return math.floor(vim.o.lines * 0.75)
-			end,
-			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
-			end,
-			icons = {
-				DEBUG = "",
-				ERROR = "",
-				INFO = "",
-				TRACE = "✎",
-				WARN = "",
-			},
+		require("notify").setup({
+			max_width = 50,
 		})
-
-		local severity = {
-			"error",
-			"warn",
-			"info",
-		}
-		vim.lsp.handlers["window/showMessage"] = function(method, params)
-			vim.notify(method.message, severity[params.type])
-		end
 	end,
 }
